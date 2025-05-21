3a9641e7b89dd53b5af2901610eb8e3c3957bac5
StevenSF1998 <steven@virtuals.io>
2025-05-21 17:33:32 +0800

    fix self.eval test

--- examples/acp_base/self_evaluation/buyer.py
+++ examples/acp_base/self_evaluation/buyer.py
@@ -5,11 +5,10 @@ import time
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from acp_sdk.utils.job_actions import evaluate_job
 from acp_sdk.client import VirtualsACP
-from acp_sdk.models import ACPJobPhase, IACPJob
+from acp_sdk.job import AcpJob
+from acp_sdk.models import ACPJobPhase
 from acp_sdk.configs import BASE_SEPOLIA_CONFIG
-from acp_sdk.utils.job_actions import pay_job
 from acp_sdk.env import EnvSettings
 
 from dotenv import load_dotenv
@@ -20,27 +19,24 @@ load_dotenv(override=True)
 def test_buyer():
     env = EnvSettings()
 
-    def on_new_task(job: IACPJob):
-        job_phase = ACPJobPhase(job.phase) if isinstance(job.phase, int) else job.phase
-        if job_phase == ACPJobPhase.NEGOTIATION:
+    def on_new_task(job: AcpJob):
+        if job.phase == ACPJobPhase.NEGOTIATION:
             # Check if there's a memo that indicates next phase is TRANSACTION
             for memo in job.memos:
-                next_phase = ACPJobPhase(memo.next_phase) if isinstance(memo.next_phase, int) else memo.next_phase
-                if next_phase == ACPJobPhase.TRANSACTION:
+                if memo.next_phase == ACPJobPhase.TRANSACTION:
                     print("Paying job", job.id)
-                    pay_job(acp, job.id, job.memos, 2)
+                    job.pay(2)
                     break
-        elif job_phase == ACPJobPhase.COMPLETED:
+        elif job.phase == ACPJobPhase.COMPLETED:
             print("Job completed", job)
     
-    def on_evaluate(job: IACPJob):
+    def on_evaluate(job: AcpJob):
         print("Evaluation function called", job.memos)
         # Find the deliverable memo
         for memo in job.memos:
-            next_phase = ACPJobPhase(memo.next_phase) if isinstance(memo.next_phase, int) else memo.next_phase
-            if next_phase == ACPJobPhase.COMPLETED:
+            if memo.next_phase == ACPJobPhase.COMPLETED:
                 # Evaluate the deliverable by accepting it
-                evaluate_job(acp, job.memos, True)
+                job.evaluate(True)
                 break
             
     acp = VirtualsACP(
